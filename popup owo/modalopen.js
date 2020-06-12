55564fa499b8c04d1442cb67787c652a431cd1b5
aaaa1234566 <taj.vanderklauw@student.education.wa.edu.au>
2020-06-12 15:22:17 +0800

    yeet
    dfge

--- popup owo/modalopen.js
+++ popup owo/modalopen.js
@@ -1,3 +1,4 @@
+var array = [[1], [2], [3]]
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -22,4 +23,35 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
+
+var modal1 = document.getElementById("myModal1");
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("myBtn1");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+}
+
+var Ivysaur = document.getElementById(0);
+
+Ivysaur.onclick = function() {
+  var number = 0
 }
