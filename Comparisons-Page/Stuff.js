5fe5f7873deadaee038619ec84e6e7e945509a95
Tobeza <Tobias3191@gmail.com>
2020-06-23 13:51:18 +0800

    yes

--- Comparisons-Page/Stuff.js
+++ Comparisons-Page/Stuff.js
@@ -1,5 +1,22 @@
-clearFunction = function () {
+clearFunction = function() {
     for (let i=0; i<gridImages.length; i++)
         gridImages[i].style.display = "none";
         health1[i]style.display = "none";
 }
+
+clearStats = function() {
+    var StartHp= 0;
+    var StartAttack = 0;
+    var StartDefense = 0;
+    var StartSpeed = 0;
+    var StartSAttack = 0;
+    var StartSDefence = 0;
+    var StartType = 0;
+    }
+
+    <h2> Attack: </h2>
+            <h2> Defense: </h2>
+            <h2> Speed: </h2>
+            <h2> Special Attack: </h2>
+            <h2> Special Defense: </h2> <h3> </h3>
+            <h2> Type: </h2> <img src = ""> 
